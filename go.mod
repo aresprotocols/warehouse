ac0100af32351accb1698760a729df68df5b045a
xjz <13456722654@139.com>
2021-09-24 15:35:56 +0800

    async request

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/jmoiron/sqlx v1.3.4 // indirect
 	github.com/pelletier/go-toml v1.9.3
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- price_server/exchange/exchange.go
+++ price_server/exchange/exchange.go
@@ -8,82 +8,97 @@ import (
 	"time"
 )
 
+var gCh = make(chan conf.PriceInfo)
+
 func GetExchangePrice(cfg conf.Config) (conf.PriceInfos, error) {
 	var retPriceInfos conf.PriceInfos
 
 	timestamp := time.Now().Unix()
 	for _, exchange := range cfg.Exchanges {
 		for _, symbol := range cfg.Symbols {
-			var priceInfo conf.PriceInfo
-			lowName := strings.ToLower(exchange.Name)
-			resJson, err := getPriceBySymbolExchange(exchange.Url, symbol, exchange.Name, cfg.Proxy)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
+			go getPriceInfo(exchange, symbol, cfg.Proxy)
+		}
+	}
 
-			priceInfo.Weight = exchange.Weight
-			priceInfo.Symbol = strings.Replace(symbol, "-", "", -1)
-			priceInfo.PriceOrigin = exchange.Name
-			priceInfo.TimeStamp = timestamp
-
-			//add price
-			var price float64
-			if lowName == "binance" {
-				price, err = parseBinancePrice(resJson)
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-			} else if lowName == "huobi" {
-				price, err = parseHuobiPrice(resJson)
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-			} else if lowName == "bitfinex" {
-				price, err = parseBitfinexPrice(resJson)
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-			} else if lowName == "ok" {
-				price, err = parseOkPrice(resJson)
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-			} else if lowName == "cryptocompare" {
-				price, err = parseCryptoComparePrice(resJson)
-				if err != nil {
-					log.Println(resJson)
-					log.Println(err)
-					continue
-				}
-			} else if lowName == "coinbase" {
-				price, err = parseCoinbasePrice(resJson)
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-			} else if lowName == "bitstamp" {
-				price, err = parseBitStampPrice(resJson)
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-			} else {
-				log.Println("unknow exchange name:", exchange.Name)
-				continue
-			}
-
-			priceInfo.Price = price
-			//log.Println(priceInfo)
+	for i := 0; i < len(cfg.Exchanges)*len(cfg.Symbols); i++ {
+		priceInfo := <-gCh
+		priceInfo.TimeStamp = timestamp
+		if priceInfo.Price != 0 {
 			retPriceInfos.PriceInfos = append(retPriceInfos.PriceInfos, priceInfo)
 		}
 	}
 
 	return retPriceInfos, nil
+}
+
+func getPriceInfo(exchange conf.ExchangeConfig, symbol string, proxy string) {
+	var priceInfo conf.PriceInfo
+	defer func() {
+		gCh <- priceInfo
+	}()
+
+	lowName := strings.ToLower(exchange.Name)
+	resJson, err := getPriceBySymbolExchange(exchange.Url, symbol, exchange.Name, proxy)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	priceInfo.Weight = exchange.Weight
+	priceInfo.Symbol = strings.Replace(symbol, "-", "", -1)
+	priceInfo.PriceOrigin = exchange.Name
+
+	//add price
+	var price float64
+	if lowName == "binance" {
+		price, err = parseBinancePrice(resJson)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	} else if lowName == "huobi" {
+		price, err = parseHuobiPrice(resJson)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	} else if lowName == "bitfinex" {
+		price, err = parseBitfinexPrice(resJson)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	} else if lowName == "ok" {
+		price, err = parseOkPrice(resJson)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	} else if lowName == "cryptocompare" {
+		price, err = parseCryptoComparePrice(resJson)
+		if err != nil {
+			log.Println(resJson)
+			log.Println(err)
+			return
+		}
+	} else if lowName == "coinbase" {
+		price, err = parseCoinbasePrice(resJson)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	} else if lowName == "bitstamp" {
+		price, err = parseBitStampPrice(resJson)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	} else {
+		log.Println("unknow exchange name:", exchange.Name)
+		return
+	}
+
+	priceInfo.Price = price
 }
 
 func getPriceBySymbolExchange(url, symbol, exchangeName, proxy string) (string, error) {
