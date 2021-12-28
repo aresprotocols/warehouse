01f076ec3c603bef42623cceac9b5d3957ba05a4
Ethan Wang <git@ethan.wang>
2021-12-28 21:06:05 +0800

    modify TestKeepValidDecimals

--- price_server/internal/util/keep_valid_decimals_test.go
+++ price_server/internal/util/keep_valid_decimals_test.go
@@ -7,16 +7,94 @@ import (
 )
 
 func TestKeepValidDecimals(t *testing.T) {
+
 	keep := 6
-
-	args := []float64{0, 1, 10, 11, 58909, 658900, 658901, 658901.01, 658901.1, 658901.001234566, 658901.000234466, 0.1, 0.01, 0.0001, 0.01234556, 0.01234454, 0.000000012345, 0.0012344, 0.000000012345789}
-	wants := []float64{0, 1, 10, 11, 58909, 658900, 658901, 658901.01, 658901.1, 658901.001235, 658901.000234, 0.1, 0.01, 0.0001, 0.0123456, 0.0123445, 0.000000012345, 0.0012344, 0.0000000123458}
-
-	for i, tt := range args {
-		dec := decimal.NewFromFloat(tt)
+	tests := []struct {
+		args float64
+		want float64
+	}{
+		{
+			args: 0,
+			want: 0,
+		},
+		{
+			args: 1,
+			want: 1,
+		},
+		{
+			args: 10,
+			want: 10,
+		},
+		{
+			args: 11,
+			want: 11,
+		},
+		{
+			args: 58909,
+			want: 58909,
+		},
+		{
+			args: 658900,
+			want: 658900,
+		},
+		{
+			args: 658901,
+			want: 658901,
+		},
+		{
+			args: 658901.01,
+			want: 658901.01,
+		},
+		{
+			args: 658901.1,
+			want: 658901.1,
+		},
+		{
+			args: 658901.001234566,
+			want: 658901.001235,
+		},
+		{
+			args: 658901.000234466,
+			want: 658901.000234,
+		},
+		{
+			args: 0.1,
+			want: 0.1,
+		},
+		{
+			args: 0.01,
+			want: 0.01,
+		},
+		{
+			args: 0.0001,
+			want: 0.0001,
+		},
+		{
+			args: 0.01234556,
+			want: 0.0123456,
+		},
+		{
+			args: 0.01234454,
+			want: 0.0123445,
+		},
+		{
+			args: 0.000000012345,
+			want: 0.000000012345,
+		},
+		{
+			args: 0.0012344,
+			want: 0.0012344,
+		},
+		{
+			args: 0.000000012345789,
+			want: 0.0000000123458,
+		},
+	}
+	for _, tt := range tests {
+		dec := decimal.NewFromFloat(tt.args)
 		t.Run(fmt.Sprintf("test %s", dec.String()), func(t *testing.T) {
-			if got := KeepValidDecimals(dec, keep); got != wants[i] {
-				t.Errorf("KeepValidDecimals() = %v, want %v", got, wants[i])
+			if got := KeepValidDecimals(dec, keep); got != tt.want {
+				t.Errorf("KeepValidDecimals() = %v, want %v", got, tt.want)
 			}
 		})
 	}
